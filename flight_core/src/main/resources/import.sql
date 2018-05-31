efde9114511ed8f6a852b8ee630fb726f56f8146
Luis Manresa <luismanresaramirez@gmail.com>
2018-05-31 16:18:58 +0200

    e

--- flight_core/src/main/resources/import.sql
+++ flight_core/src/main/resources/import.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 INSERT INTO Localite(id , ville , aeroport)  VALUES ( 1 , 'Paris' , 'Paris-Orly' )
 INSERT INTO Localite(id , ville , aeroport)  VALUES ( 2 , 'Paris' , 'Paris-Roissy' )
 INSERT INTO Localite(id , ville , aeroport)  VALUES ( 3 , 'Toulouse' , 'Toulouse-Blagnac' )
@@ -9,16 +8,7 @@ INSERT INTO Vol(num , prix , refLocDepart,dateTimeDepart, refLocArrivee,dateTime
 INSERT INTO Vol(num , prix , refLocDepart,dateTimeDepart, refLocArrivee,dateTimeArrivee) VALUES ( 3 , 80.0  , 1 ,  '2018-09-20 12:20:00' , 4 , '2018-09-20 13:30:00' )
 INSERT INTO Vol(num , prix , refLocDepart,dateTimeDepart, refLocArrivee,dateTimeArrivee) VALUES ( 4 , 70.0  , 4 ,  '2018-09-20 20:30:00' , 1 , '2018-09-20 21:15:00' ) 
 
+
 INSERT INTO PERSONNE (TYPEPERSONNE, ID, EMAIL, NOM, PRENOM, TELEPHONE, CODEPOSTAL, NUMETRUE, PAYS, VILLE) VALUES ('CLI', 1, 'yvessav@free.fr', 'savang', 'yves', '0145253573', '75007', '8 villa de Ségur', 'France', 'Paris')
 INSERT INTO PERSONNE (TYPEPERSONNE, ID, EMAIL, NOM, PRENOM, TELEPHONE, CODEPOSTAL, NUMETRUE, PAYS, VILLE) VALUES ('CLI', 2, 'babel@free.fr', 'joris', 'boris', '0620020630', '89255', '19 rue de la Perche', 'France', 'Perpete')
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
 INSERT INTO PERSONNE (TYPEPERSONNE, ID, EMAIL, NOM, PRENOM, TELEPHONE, CODEPOSTAL, NUMETRUE, PAYS, VILLE) VALUES ('CLI', 3, 'babel@free.fr', 'joris', 'boris', '0620020630', '89255', '19 rue de la Perche', 'France', 'Perpete')
-=======
-)
-)
->>>>>>> 57e0d24652e043f4df078d65261f471a771f0524
->>>>>>> 8fe79cbf0134e4a03bb14433295618a4f29f69ff
->>>>>>> 2524311bd07c44ce5eb0e0e095cbc3da77e52283
