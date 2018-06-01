7f0800508a6d4c15105eb445611bd20e0be4219e
Luis Manresa <luismanresaramirez@gmail.com>
2018-06-01 15:09:16 +0200

    b

--- flight_core/src/test/java/com/m2i/test/ServiceResaTest.java
+++ flight_core/src/test/java/com/m2i/test/ServiceResaTest.java
@@ -1,5 +1,8 @@
 package com.m2i.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +10,39 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.m2i.dao.IReservationDAO;
+import com.m2i.entity.Reservation;
+import com.m2i.entity.client.Adresse;
+import com.m2i.entity.client.Client;
+import com.m2i.entity.client.Personne;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/beans.xml")
 public class ServiceResaTest {
 	@Autowired
-	private IReservationDAO reservation;
+	private IReservationDAO daoRes;
 	
 	@Test
 	public void testNouvelleRSA() {
+		String nom = "Dupont";
+		String prenom = "Dimitri";
+		String mail="dimitri@gmail.com";
+		String tel ="0123456789";
+		Adresse adresse = new Adresse("45 rue du Maine", "981247", "Lyon", "France");
+		Client client = new Client(nom,prenom,mail,tel, adresse);
+		Reservation reservation = new Reservation();
+		reservation.setClient(client);
+		Personne personne = new Personne();
+		personne.setNom(nom);
+		personne.setPrenom(prenom);
+		personne.setEmail(mail);
+		personne.setTelephone(tel);
+		personne.setAdresse(adresse);
+		List <Personne> listePersonnes = new ArrayList<Personne>();
+		listePersonnes.add(personne);
+		reservation.setPassagers(listePersonnes);
+		
+		daoRes.createResa(reservation);
+
 		
 	}
 }
